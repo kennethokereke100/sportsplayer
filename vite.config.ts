848d71ea910219a3d2b904f612c978add2e25405
Kenneth Okereke <kennethokereke@Kenneths-MacBook-Air.local>
2025-07-22 20:25:58 -0400

    deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/sports-player-lookup/', // replace with your repo name
+  plugins: [react()]
 })
